4c05b901d41b822f6fbfcdb5316df4cbdf8b2ebd
Rene Jochum <from_github@pc-dummy.net>
2011-04-27 19:25:20 +0200

    Updates the DB template.

--- data/postgres.sql
+++ data/postgres.sql
@@ -58,6 +58,49 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: sessions; Type: TABLE; Schema: public; Owner: wardbu; Tablespace: 
+--
+
+CREATE TABLE sessions (
+    id bigint NOT NULL,
+    username character varying(255) NOT NULL,
+    session character varying(16) NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    created_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.sessions OWNER TO wardbu;
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: wardbu
+--
+
+CREATE SEQUENCE sessions_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.sessions_id_seq OWNER TO wardbu;
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: wardbu
+--
+
+ALTER SEQUENCE sessions_id_seq OWNED BY sessions.id;
+
+
+--
+-- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: wardbu
+--
+
+SELECT pg_catalog.setval('sessions_id_seq', 1, true);
+
+
+--
 -- Name: tokens; Type: TABLE; Schema: public; Owner: wardbu; Tablespace: 
 --
 
@@ -66,7 +109,8 @@ CREATE TABLE tokens (
     username character varying(255) NOT NULL,
     token character varying(32) NOT NULL,
     created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    updated_at timestamp without time zone NOT NULL,
+    last_ip character varying(40) NOT NULL
 );
 
 
@@ -104,31 +148,61 @@ SELECT pg_catalog.setval('token_id_seq', 1, true);
 -- Name: id; Type: DEFAULT; Schema: public; Owner: wardbu
 --
 
+ALTER TABLE sessions ALTER COLUMN id SET DEFAULT nextval('sessions_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: wardbu
+--
+
 ALTER TABLE tokens ALTER COLUMN id SET DEFAULT nextval('token_id_seq'::regclass);
+
+
+--
+-- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: wardbu
+--
+
+COPY sessions (id, username, session, updated_at, created_at) FROM stdin;
+\.
 
 
 --
 -- Data for Name: tokens; Type: TABLE DATA; Schema: public; Owner: wardbu
 --
 
-COPY tokens (id, username, token, created_at, updated_at) FROM stdin;
+COPY tokens (id, username, token, created_at, updated_at, last_ip) FROM stdin;
 \.
 
 
 --
--- Name: primary_id; Type: CONSTRAINT; Schema: public; Owner: wardbu; Tablespace: 
+-- Name: sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: wardbu; Tablespace: 
+--
+
+ALTER TABLE ONLY sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: tokens_pkey; Type: CONSTRAINT; Schema: public; Owner: wardbu; Tablespace: 
 --
 
 ALTER TABLE ONLY tokens
-    ADD CONSTRAINT primary_id PRIMARY KEY (id);
+    ADD CONSTRAINT tokens_pkey PRIMARY KEY (id);
 
 
 --
--- Name: username_unique_id; Type: CONSTRAINT; Schema: public; Owner: wardbu; Tablespace: 
+-- Name: tokens_username_token_unique_key; Type: CONSTRAINT; Schema: public; Owner: wardbu; Tablespace: 
 --
 
 ALTER TABLE ONLY tokens
-    ADD CONSTRAINT username_unique_id UNIQUE (username);
+    ADD CONSTRAINT tokens_username_token_unique_key UNIQUE (username, token);
+
+
+--
+-- Name: sessions_username_session_idx; Type: INDEX; Schema: public; Owner: wardbu; Tablespace: 
+--
+
+CREATE INDEX sessions_username_session_idx ON sessions USING btree (username, session);
 
 
 --
@@ -139,20 +213,40 @@ CREATE RULE update_token AS ON INSERT TO tokens WHERE (EXISTS (SELECT 1 FROM tok
 
 
 --
--- Name: set_create_times_tr; Type: TRIGGER; Schema: public; Owner: wardbu
+-- Name: sessions_created_at; Type: TRIGGER; Schema: public; Owner: wardbu
 --
 
-CREATE TRIGGER set_create_times_tr
+CREATE TRIGGER sessions_created_at
+    BEFORE INSERT ON sessions
+    FOR EACH ROW
+    EXECUTE PROCEDURE set_create_times();
+
+
+--
+-- Name: sessions_updated_at; Type: TRIGGER; Schema: public; Owner: wardbu
+--
+
+CREATE TRIGGER sessions_updated_at
+    BEFORE UPDATE ON sessions
+    FOR EACH ROW
+    EXECUTE PROCEDURE set_update_time();
+
+
+--
+-- Name: tokens_created_at; Type: TRIGGER; Schema: public; Owner: wardbu
+--
+
+CREATE TRIGGER tokens_created_at
     BEFORE INSERT ON tokens
     FOR EACH ROW
     EXECUTE PROCEDURE set_create_times();
 
 
 --
--- Name: set_update_time_tr; Type: TRIGGER; Schema: public; Owner: wardbu
+-- Name: tokens_updated_at; Type: TRIGGER; Schema: public; Owner: wardbu
 --
 
-CREATE TRIGGER set_update_time_tr
+CREATE TRIGGER tokens_updated_at
     BEFORE UPDATE ON tokens
     FOR EACH ROW
     EXECUTE PROCEDURE set_update_time();
